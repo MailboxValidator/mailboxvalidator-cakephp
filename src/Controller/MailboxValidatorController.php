509f3af71ce9ea3db4b27a973776cacd4504a447
MailboxValidator <support@mailboxvalidator.com>
2019-01-16 13:33:38 +0800

    Update MailboxValidatorController.php

--- src/Controller/MailboxValidatorController.php
+++ src/Controller/MailboxValidatorController.php
@@ -27,8 +27,9 @@ class MailboxValidatorController{
 
 	public function disposable ($email) {
 		$api_key = Configure::read('MBV_API_KEY');
+		$source = 'cakephp';
 		if (trim($email) != '') {
-			$results = file_get_contents('https://api.mailboxvalidator.com/v1/email/disposable?key=' . $api_key . '&email=' .$email);
+			$results = file_get_contents('https://api.mailboxvalidator.com/v1/email/disposable?key=' . $api_key . '&email=' .$email. '&source=' .$source );
 			// Decode the return json results and return the data as an array.
 			$data = json_decode($results,true);
 			if (trim ($data['error_code']) == '' ) {
@@ -47,8 +48,9 @@ class MailboxValidatorController{
 
 	public function free($email){
 		$api_key = Configure::read('MBV_API_KEY');
+		$source = 'cakephp';
 		if (trim($email) != '') {
-			$results = file_get_contents('https://api.mailboxvalidator.com/v1/email/free?key=' . $api_key . '&email=' .$email);
+			$results = file_get_contents('https://api.mailboxvalidator.com/v1/email/free?key=' . $api_key . '&email=' .$email. '&source=' .$source );
 			// Decode the return json results and return the data as an array.
 			$data = json_decode($results,true);
 			if (trim ($data['error_code']) == '' ) {
